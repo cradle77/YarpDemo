b4f869804360e2f1521691f22ce7912a2c98f673
MarcoDes <marcodesanctis2@gmail.com>
2022-05-17 19:16:48 +0100

    Add project files.

---
+++ Demo-API/Program.cs
@@ -0,0 +1,29 @@
+namespace Demo_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseCors(configurePolicy => 
+            {
+                configurePolicy.AllowAnyOrigin();
+            });
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
