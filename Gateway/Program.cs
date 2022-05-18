16aaace464717da03786282f35cadd1a91571175
MarcoDes <marcodesanctis2@gmail.com>
2022-05-18 10:56:00 +0100

    base

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -9,10 +9,25 @@ namespace Gateway
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+            builder.Services.AddAuthorization(configure =>
+            {
+                configure.AddPolicy("authenticated", policy => policy.RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(OpenIdConnectDefaults.AuthenticationScheme));
+
+                configure.AddPolicy("authenticatedApi", policy => policy.RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes("Cookies"));
+            });
+
             builder.Services.AddReverseProxy()
                 .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
             
             var app = builder.Build();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapReverseProxy();
             app.Run();
