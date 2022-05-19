b0d610887585379edd4efe97ec8d4087e2894c75
MarcoDes <marcodesanctis2@gmail.com>
2022-05-19 17:38:22 +0100

    removed cluster security

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -21,18 +21,6 @@ namespace Gateway
                 configure.AddPolicy("authenticatedApi", policy => policy.RequireAuthenticatedUser()
                     .AddAuthenticationSchemes("Cookies"));
             });
-
-            var cert = new X509Certificate2(Path.Combine(AppContext.BaseDirectory, "client.pfx"), "1234");
-
-            builder.Services.AddReverseProxy()
-                .ConfigureHttpClient((context, handler) =>
-                {
-                    handler.SslOptions.ClientCertificates = new X509CertificateCollection
-                    (
-                        new[] { cert }
-                    );
-                })
-                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
             
             var app = builder.Build();
 
